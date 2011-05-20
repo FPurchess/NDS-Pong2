6eb0370e1cfa917c5b51109e9e72903b70068515
Florian Purchess <florian@purchess.de>
2011-05-20 15:41:28 +0200

    inital startup

---
+++ source/main.cpp
@@ -0,0 +1,30 @@
+#include <nds.h>
+#include <stdio.h>
+
+#define LCD_W 256
+#define LCD_H 192
+
+
+int main(void) {
+    int i;
+
+	consoleDemoInit();
+
+	videoSetMode(MODE_FB0);
+
+	vramSetBankA(VRAM_A_LCD);
+
+	printf("Hello World!");
+
+	for(i = 0; i < 256 * 192; i++) {
+		VRAM_A[i] = RGB15(31, 0, 0);
+	}
+
+	while(1) {
+		swiWaitForVBlank();
+    
+        //consoleClear();
+	}   
+
+    return 0;
+}

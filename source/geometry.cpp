b8f6fda67af544af08be4ba59613ed405f6a7b8f
Florian Purchess <florian@purchess.de>
2011-06-06 13:59:20 +0200

    basic collision implemented

--- source/geometry.cpp
+++ source/geometry.cpp
@@ -2,13 +2,40 @@
 
 
 /**
- * checks two boundingBoxes of collision
+ * checks if box2 collides with box1
  * 
  * @param boundingBox box1
  * @param boundingBox box2
- * @return boolean
+ * @return int 0 no collision, 1-4 clockwise collision
  */
-bool collision(boundingBox box1, boundingBox box2) {
+int collision(boundingBox box1, boundingBox box2) {
+    // top
+    if (box1.pos.x <= box2.pos.x && box1.pos.x + box1.height >= box2.pos.x) {
+        if (box1.pos.y <= box2.pos.y && box1.pos.y + box1.height >= box2.pos.y + box2.height) {
+            return 1;
+        }
+    }
     
-    return false;
+    // right
+    if (box1.pos.x <= box2.pos.x && box1.pos.x + box1.width >= box2.pos.x) {
+        if (box1.pos.y <= box2.pos.y && box1.pos.y + box1.height >= box2.pos.y) {
+            return 2;
+        }
+    }
+
+    // bottom
+    if (box1.pos.x <= box2.pos.x && box1.pos.x + box1.width >= box2.pos.x + box2.width) {
+        if (box1.pos.y + box1.height >= box2.pos.y && box1.pos.y <= box2.pos.y) {
+            return 3;
+        }
+    }
+
+    //left
+    if (box1.pos.x >= box2.pos.x + box2.width && box1.pos.x + box1.width <= box2.pos.x) {
+        if (box1.pos.y <= box2.pos.y + box2.height && box1.pos.y + box1.height >= box2.pos.y) {
+            return 4;
+        }
+    }
+
+    return 0;
 }

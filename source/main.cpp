5beaaa3033f993406e94c66cf99f43b2700acf40
Tobias Dreyer <tewd@tewd.(none)>
2011-06-07 12:03:56 +0200

    * testing stats

--- source/main.cpp
+++ source/main.cpp
@@ -3,9 +3,12 @@
 
 #include "panel.h"
 #include "ball.h"
+#include "stats.h"
 
 #define LCD_W 256
 #define LCD_H 192
+
+#define ERR_INIT_SBOX_FAILED 1
 
 
 int main(void) {
@@ -14,16 +17,26 @@ int main(void) {
     panel player1;
     panel player2;
     ball gameBall;
+    
+    scoreBox sBox;
+    if(!initScoreBox(&sBox))
+        exit(ERR_INIT_SBOX_FAILED);
 
-    consoleDemoInit();
+    int score[2] = {1,2};
+    
+    setScore(&sBox, score);
+
+//    consoleDemoInit();
 
 	videoSetMode(MODE_FB0);
 
 	vramSetBankA(VRAM_A_LCD);
 
 	for(i = 0; i < 256 * 192; i++) {
-		VRAM_A[i] = RGB15(31, 0, 0);
+		VRAM_B[i] = RGB15(31, 0, 0);
 	}
+	
+	printf("%d : %d",sBox.score[0],sBox.score[1]);
 
 	while(1) {
 		swiWaitForVBlank();

086a8488f9a8054d0492ff6e7cf5c88e179bb049
Florian Purchess <florian@purchess.de>
2011-06-25 17:36:04 +0200

    refactoring

--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,65 @@
 #include <nds.h>
+#include <stdio.h>
 
-#include "scene_mainmenu.h"
 #include "mode_oneplayer.h"
 #include "mode_twoplayer.h"
 
+#define GAME_MODE_LENGTH 1
+
+typedef struct {
+    char *name;
+    void (*funcp)(void);
+} funcmap;
+
+funcmap gameModes[] = {
+    {"1 Player", mode_oneplayer},
+    {"2 Player", mode_twoplayer}
+};
+
+
+//@TODO replace console with backgrounds
+
+void initMainmenuVideo() {
+    consoleDemoInit();
+    videoSetMode(MODE_5_2D | DISPLAY_BG2_ACTIVE);
+}
+
+void printMainmenu(int mode) {
+    printf("\n\n\t\tM A I N M E N U\n\n");
+    for(int i=0;i<=GAME_MODE_LENGTH;i++) {
+        printf("\t\t%c %s\n\n", i==mode ? '*' : ' ', gameModes[i].name);
+    }
+}
 
 int main(void) {
-    int gameMode;
+    int i, held = 0, mode = 0;
     
     powerOn(POWER_ALL);
     lcdSwap();
 
-    while(1) {
-        gameMode = mainmenu();
+    initMainmenuVideo();
 
-        switch (gameMode) {
-            case 0:
-                mode_oneplayer();
-                break;
-            case 1:
-                mode_twoplayer();
-                break;
+    while(1) {
+        consoleClear();
+        printMainmenu(mode);
+    
+        scanKeys();
+        held = keysHeld();
+
+        if (held & KEY_UP && mode > 0) {
+            mode--;
+
+        } else if (held & KEY_DOWN && mode < GAME_MODE_LENGTH) {
+            mode++;
+
+        } else if (held & KEY_B) {
+            gameModes[mode].funcp();
+            initMainmenuVideo();
         }
+
+        //@FIXME remove this to gain speed again
+        for(i=0;i<5;i++)
+            swiWaitForVBlank();
 	}
 
     return 0;
